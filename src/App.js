f5b6755492b50c6f533172db9652dd96c77c265a
Lucia CL <luciacoucelamas@gmail.com>
2019-04-01 16:19:44 +0100

    Add your favourite movie cover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import logo from './mib.jpg';
 import './App.css';
 
 class App extends Component {
