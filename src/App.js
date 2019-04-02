907e7873521b204f138cf654836e5bf2ffedd8f1
Lucia CL <luciacoucelamas@gmail.com>
2019-04-02 10:25:22 +0100

    Add 6 TV show tit #3 and Rep TV s tit with cove #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import image from './mib.jpg';
+import image from './covers/mib.jpg';
+import blackMirror from './covers/bmcover.jpg';
+import breakingBad from './covers/brbcover.jpg';
+import deathNote from './covers/dncover.jpg';
+import gameOfThrones from './covers/gotcover.jpeg';
+import theWire from './covers/twcover.jpeg';
+import walkingDead from './covers/wdcover.jpg';
+
+
 import './App.css';
 
 class App extends Component {
@@ -8,6 +16,29 @@ class App extends Component {
       <div className="App">
         <h1>Welcome to Kodflix</h1>
         <img src={image} alt="first cover"/>
+        <br/>
+        <div className='container'>
+          <div className='item'>
+            <img src={blackMirror} alt="first cover"/>
+          </div>
+          <div className='item'>
+            <img src={breakingBad} alt="first cover"/>
+          </div>
+          <div className='item'>
+            <img src={deathNote} alt="first cover"/>
+          </div>
+        </div>
+        <div className='container' >
+          <div className='item'>
+            <img src={gameOfThrones} alt="first cover"/>
+          </div>
+          <div className='item'>
+            <img src={walkingDead} alt="first cover"/>
+          </div>
+          <div className='item'>
+            <img src={theWire} alt="first cover"/>
+          </div>
+        </div>
       </div>
     );
   }
