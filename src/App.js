74a66bc714f1d6d6295645b7d3b83e790462a647
Lucia CL <luciacoucelamas@gmail.com>
2019-04-03 11:06:36 +0100

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import image from './covers/mib.jpg';
 import blackMirror from './covers/bmcover.jpg';
-import breakingBad from './covers/brbcover.jpg';
+import breakingBad from './covers/bbcover.jpg';
 import deathNote from './covers/dncover.jpg';
-import gameOfThrones from './covers/gotcover.jpeg';
-import theWire from './covers/twcover.jpeg';
+import gameOfThrones from './covers/gotcover.png';
+import theWire from './covers/wwcover.jpg';
 import walkingDead from './covers/wdcover.jpg';
-
 
 import './App.css';
 
@@ -20,29 +19,29 @@ class App extends Component {
           <div className='container'>
             <div className='item' >
               <img  src={blackMirror} alt="black mirror cover"/>
-              <h1 className="myH1Style">Black Mirror</h1>
+              <div className="titleMovie"><h1 >Black Mirror</h1></div>
             </div>
             <div className='item'>
               <img src={breakingBad} alt="breaking bad cover"/>
-              <h1 className='myH1Style'> Breaking Bad</h1>  
+              <h1 className='titleMovie'> Breaking Bad</h1>  
             </div>
             <div className='item'>
               <img src={deathNote} alt="death note cover"/>
-              <h1 className='myH1Style'>Death Note</h1>  
+              <h1 className='titleMovie'>Death Note</h1>  
             </div>
           </div>
           <div className='container' >
             <div className='item'>
               <img src={gameOfThrones} alt="game of thrones cover"/>
-              <h1 className='myH1Style'> Game of Thrones</h1>  
+              <h1 className='titleMovie'> Game of Thrones</h1>  
             </div>
             <div className='item'>
               <img src={walkingDead} alt="walking dead cover"/>
-              <h1 className='myH1Style'> Walking Dead</h1>  
+              <h1 className='titleMovie'> Walking Dead</h1>  
             </div>
             <div className='item'>
               <img src={theWire} alt="the wire cover"/>
-              <h1 className='myH1Style'>The wire</h1>  
+              <h1 className='titleMovie'>Wonder Woman</h1>  
             </div>
           </div>
       </div>
