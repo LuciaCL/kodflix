e34cbc0a389deb0ebd2875872ed8fd77d65fac30
Lucia CL <luciacoucelamas@gmail.com>
2019-04-09 16:45:04 +0100

    Improve the gallery layout #11

--- src/Gallery.js
+++ src/Gallery.js
@@ -13,9 +13,7 @@ export default function Gallery(props){
             <div className='container'>
                 <Stack name="Black Mirror" source={blackMirror} title="black mirror cover" id='BlackMirror'></Stack>
                 <Stack name="Breaking Bad" source={breakingBad} title="breaking bad cover" id='BreakingBad'></Stack>
-                <Stack name="Death Note" source={deathNote} title="death note cover" id='DeathNote'></Stack>
-            </div>
-            <div className='container' >
+                <Stack name="Death Note" source={deathNote} title="death note cover" id='DeathNote'></Stack>         
                 <Stack name="Game of Thrones" source={gameOfThrones} title="game of thrones cover" id='GameOfThrones'></Stack>
                 <Stack name="Walking Dead" source={walkingDead} title="walking dead cover" id='WalkingDead'></Stack>
                 <Stack name="Wonder Woman" source={wonderWoman} title="wonder woman cover" id='WonderWoman'></Stack>
