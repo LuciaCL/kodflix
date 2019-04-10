7d8912ff06c606e619cff96fb28b7e2049e318e7
Lucia CL <luciacoucelamas@gmail.com>
2019-04-10 11:10:34 +0100

    Introduce component state #13

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
-    return (
-        <div>
-            <h1> Welcome to the details page, WIP :)</h1>
-            <Link to='/'>Back to home page</Link>
-        </div>
-    );
+export default class Details extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message : "Hello, this will be the details page for each Movie & TV show :)"
+
+        }
+    }
+    componentDidMount(){
+        setTimeout(() => {
+           this.setState({
+               message: 'Coming soon! :)'
+           }) 
+        }, 3000);
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <Link to='/'>Back to home page</Link>
+            </div>
+        );
+    }
 }
 
