d39708830a51deba23ef61717fb197ba9dcdc7c5
Lucia CL <luciacoucelamas@gmail.com>
2019-04-11 17:03:06 +0100

    Show the TV show title on the details page #14

--- src/Details.js
+++ src/Details.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import getTvShows from './TvShows-get.js';
 
 export default class Details extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            message : "Hello, this will be the details page for each Movie & TV show :)"
-
+            tvShow: {}
         }
     }
+
     componentDidMount(){
+        let tvShowId = this.props.match.params.tvShowId;
+        let tvShow = getTvShows()
+            .find((tvShow) =>tvShow.id === tvShowId);
         setTimeout(() => {
-           this.setState({
-               message: 'Coming soon! :)'
-           }) 
-        }, 3000);
+           this.setState({ tvShow }) 
+        });
     }
+
     render() {
         return (
             <div>
-                <h1>{this.state.message}</h1>
+                <h1>{this.state.tvShow.name}</h1>
                 <Link to='/'>Back to home page</Link>
             </div>
         );
