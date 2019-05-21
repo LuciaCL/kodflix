0ed77805279a14203492f42cdf6af5655b618719
Lucia CL <luciacoucelamas@gmail.com>
2019-05-21 12:57:09 +0100

    Connect the kodflix app with our database! #28

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,12 +4,22 @@ const app = express();
 const port = process.env.PORT || 3001;
 const getShows = require('./shows.js');
 const pathToBuildFolder = '../../build';
+const db = require('./db');
 
-app.get('/rest/shows', (req, res) => res.send(getShows()));
+//app.get('/rest/shows', (req, res) => res.send(getShows()));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-app.use(express.static(path.join(__dirname, pathToBuildFolder)));
+db.connect().then(dbo => {
+    app.get('/rest/shows', (req, res) => {
+        dbo.collection('shows').find({}).toArray((err, results) => {
+            if (err) throw err;
+            res.send(results);
+        });
+    });
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, pathToBuildFolder, 'index.html'));
+    app.use(express.static(path.join(__dirname, pathToBuildFolder)));
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, pathToBuildFolder, 'index.html'));
+    });
 });
+
